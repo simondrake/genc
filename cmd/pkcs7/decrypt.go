e2091dbb7e3e32be953b35b5d868222827345ae6
Simon Drake <simondrake1990@gmail.com>
2024-06-27 10:34:46 +0100

    [pkcs7] Add encrypt command and tests

--- cmd/pkcs7/decrypt.go
+++ cmd/pkcs7/decrypt.go
@@ -17,14 +17,26 @@ func newDecryptCommand() *cobra.Command {
 		encString  string
 		publicKey  string
 		privateKey string
-		base64     bool
+		b64        bool
 	)
 
 	decryptCmd := &cobra.Command{
 		Use:   "decrypt",
 		Short: "decrypt pkcs7 secret",
 		Run: func(cmd *cobra.Command, args []string) {
-			b, err := decryptPKCS7(encString, publicKey, privateKey, base64)
+			privKey, err := os.ReadFile(privateKey)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, fmt.Errorf("error reading private key: %w", err))
+				os.Exit(1)
+			}
+
+			pubKey, err := os.ReadFile(publicKey)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, fmt.Errorf("error reading public key: %w", err))
+				os.Exit(1)
+			}
+
+			b, err := decryptPKCS7(privKey, pubKey, b64, encString)
 			if err != nil {
 				fmt.Fprintln(os.Stderr, fmt.Errorf("error decrypting string: %w", err))
 				os.Exit(1)
@@ -37,36 +49,22 @@ func newDecryptCommand() *cobra.Command {
 	decryptCmd.Flags().StringVar(&encString, "string", "", "the encrypted string")
 	decryptCmd.Flags().StringVar(&publicKey, "public-key", "", "the location of the public key on disk")
 	decryptCmd.Flags().StringVar(&privateKey, "private-key", "", "the location of the private key on disk")
-	decryptCmd.Flags().BoolVar(&base64, "base64", true, "whether the encrypted string is base64 encoded")
+	decryptCmd.Flags().BoolVar(&b64, "base64", true, "whether the encrypted string is base64 encoded")
 
-	decryptCmd.MarkFlagRequired("string")
-	decryptCmd.MarkFlagRequired("public-key")
-	decryptCmd.MarkFlagRequired("private-key")
+	if err := decryptCmd.MarkFlagRequired("string"); err != nil {
+		fmt.Fprintln(os.Stderr, fmt.Errorf("internal error marking flag 'string' as required: %w", err))
+	}
+	if err := decryptCmd.MarkFlagRequired("public-key"); err != nil {
+		fmt.Fprintln(os.Stderr, fmt.Errorf("internal error marking flag 'public-key' as required: %w", err))
+	}
+	if err := decryptCmd.MarkFlagRequired("private-key"); err != nil {
+		fmt.Fprintln(os.Stderr, fmt.Errorf("internal error marking flag 'private-key' as required: %w", err))
+	}
 
 	return decryptCmd
 }
 
-func decryptPKCS7(enc string, pubKey string, privKey string, b64 bool) ([]byte, error) {
-	bPriv, err := os.ReadFile(privKey)
-	if err != nil {
-		return nil, fmt.Errorf("error reading private key: %w", err)
-	}
-
-	bPub, err := os.ReadFile(pubKey)
-	if err != nil {
-		return nil, fmt.Errorf("error reading public key: %w", err)
-	}
-
-	pemPriv, _ := pem.Decode(bPriv)
-	if pemPriv == nil {
-		return nil, errors.New("unable to decode private key")
-	}
-
-	pemPub, _ := pem.Decode(bPub)
-	if pemPub == nil {
-		return nil, errors.New("unable to decode public key")
-	}
-
+func decryptPKCS7(privKey []byte, pubKey []byte, b64 bool, enc string) ([]byte, error) {
 	p7b := []byte(enc)
 	if b64 {
 		var err error
@@ -79,6 +77,16 @@ func decryptPKCS7(enc string, pubKey string, privKey string, b64 bool) ([]byte, 
 	p7, err := pkcs7.Parse(p7b)
 	if err != nil {
 		return nil, fmt.Errorf("error parsing encrypted string: %w", err)
+	}
+
+	pemPriv, _ := pem.Decode(privKey)
+	if pemPriv == nil {
+		return nil, errors.New("unable to decode private key")
+	}
+
+	pemPub, _ := pem.Decode(pubKey)
+	if pemPub == nil {
+		return nil, errors.New("unable to decode public key")
 	}
 
 	x509Priv, err := x509.ParsePKCS1PrivateKey(pemPriv.Bytes)
---
+++ cmd/pkcs7/decrypt_test.go
@@ -0,0 +1,84 @@
+package pkcs7
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/base64"
+	"encoding/pem"
+	"math/big"
+	"testing"
+	"time"
+
+	"github.com/fullsailor/pkcs7"
+)
+
+func TestDecryptPKCS7(t *testing.T) {
+	t.Run("PKCS1", func(t *testing.T) {
+		plaintext := "thisissupersecret!@$%#"
+
+		// Generate new Private Key
+		privateKey, err := rsa.GenerateKey(rand.Reader, 4096)
+		if err != nil {
+			t.Errorf("GenerateKey returned an error when one wasn't expected: %+v", err)
+		}
+
+		// Generate a new X509 Certificate Template
+		tmpl, err := certTemplate()
+		if err != nil {
+			t.Errorf("certTemplate returned an error when one wasn't expected: %+v", err)
+		}
+
+		// Create Certificate DER
+		der, err := x509.CreateCertificate(rand.Reader, tmpl, tmpl, &privateKey.PublicKey, privateKey)
+		if err != nil {
+			t.Errorf("CreateCertificate returned an error when one wasn't expected: %+v", err)
+		}
+
+		// Convert Public Key to X509
+		x509PublicCert, err := x509.ParseCertificates(der)
+		if err != nil {
+			t.Errorf("ParseCertificate returned an error when one wasn't expected: %+v", err)
+		}
+
+		// Encrypt our plaintext value
+		enc, err := pkcs7.Encrypt([]byte(plaintext), x509PublicCert)
+		if err != nil {
+			t.Errorf("Encrypt returned an error when one wasn't expected: %+v", err)
+		}
+
+		// The actual test
+		out, err := decryptPKCS7(
+			pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(privateKey)}),
+			pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: der}),
+			true,
+			base64.StdEncoding.EncodeToString(enc),
+		)
+		if err != nil {
+			t.Errorf("decryptPKCS7 returned an error when one wasn't expected: %+v", err)
+		}
+
+		if string(out) != plaintext {
+			t.Errorf("result of decryptPKCS7 was expected to be '%s' but was '%s'", plaintext, string(out))
+		}
+	})
+}
+
+func certTemplate() (*x509.Certificate, error) {
+	limit := new(big.Int).Lsh(big.NewInt(1), 128)
+
+	sn, err := rand.Int(rand.Reader, limit)
+	if err != nil {
+		return nil, err
+	}
+
+	return &x509.Certificate{
+		SerialNumber:          sn,
+		Subject:               pkix.Name{Organization: []string{"Wibble Wobble, Inc."}},
+		SignatureAlgorithm:    x509.SHA256WithRSA,
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(1, 0, 0), // valid for a year
+		BasicConstraintsValid: true,
+	}, nil
+}
