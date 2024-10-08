2e0754b33382a965fdc1bb93097fbf68bdae20d6
Simon Drake <simondrake1990@gmail.com>
2024-10-08 16:06:41 +0100

    [cidr] Change overlap command to print every overlap and accept json

--- cmd/cidr/overlap.go
+++ cmd/cidr/overlap.go
@@ -1,6 +1,7 @@
 package cidr
 
 import (
+	"encoding/json"
 	"fmt"
 	"net"
 	"os"
@@ -9,33 +10,33 @@ import (
 )
 
 func newOverlapCommand() *cobra.Command {
-	var cidrs []string
+	var cidrs string
 
 	overlapCmd := &cobra.Command{
 		Use:   "overlap",
 		Short: "determine if CIDR blocks overlap with each other",
 		Example: `
-    $ genc cidr overlap --cidrs 87.243.24.122/32,87.243.24.0/24
-    CIDRs ([87.243.24.122/32 87.243.24.0/24]) do overlap
+    $ genc cidr overlap --cidrs '["87.243.24.122/32", "87.243.24.0/24"]'
+    CIDRs (87.243.24.122/32) and (87.243.24.0/24) overlap
 
-    $ genc cidr overlap --cidrs 87.243.24.122/32,87.243.25.0/24
+    $ genc cidr overlap --cidrs '["87.243.24.122/32", "87.243.25.0/24"]'
     CIDRs do not overlap`,
 		Run: func(cmd *cobra.Command, args []string) {
-			overlap, c, err := doCIDRsOverlap(cidrs)
-			if err != nil {
-				fmt.Fprintln(os.Stderr, fmt.Errorf("error determining if CIDRs overlap: %w", err))
+			var c []string
+
+			if err := json.Unmarshal([]byte(cidrs), &c); err != nil {
+				fmt.Fprintln(os.Stderr, fmt.Errorf("error parsing cidrs: %w", err))
 				os.Exit(1)
 			}
 
-			if overlap {
-				fmt.Fprintf(os.Stdout, "CIDRs (%s) do overlap", c)
-			} else {
-				fmt.Fprintln(os.Stdout, "CIDRs do not overlap")
+			if err := doCIDRsOverlap(c); err != nil {
+				fmt.Fprintln(os.Stderr, fmt.Errorf("error determining if CIDRs overlap: %w", err))
+				os.Exit(1)
 			}
 		},
 	}
 
-	overlapCmd.Flags().StringSliceVar(&cidrs, "cidrs", nil, "the list of CIDRs to check for overlap")
+	overlapCmd.Flags().StringVar(&cidrs, "cidrs", "", "the list of CIDRs to check for overlap")
 
 	if err := overlapCmd.MarkFlagRequired("cidrs"); err != nil {
 		fmt.Fprintln(os.Stderr, fmt.Errorf("internal error marking flag 'cidrs' as required: %w", err))
@@ -44,11 +45,19 @@ func newOverlapCommand() *cobra.Command {
 	return overlapCmd
 }
 
-func doCIDRsOverlap(cidrs []string) (bool, []string, error) {
+func doCIDRsOverlap(cidrs []string) error {
+	var overlaps bool
+
+	// This map is a crufty way of ensuring that we don't print out the same
+	// CIDR overlap twice (as we loop through each thing twice)
+	//
+	// Should find a more elegant way of doing this, but it'll do for now
+	processed := make(map[string]struct{})
+
 	for i1, c1 := range cidrs {
 		_, n1, err := net.ParseCIDR(c1)
 		if err != nil {
-			return false, nil, err
+			return err
 		}
 
 		for i2, c2 := range cidrs {
@@ -58,15 +67,26 @@ func doCIDRsOverlap(cidrs []string) (bool, []string, error) {
 
 			_, n2, err := net.ParseCIDR(c2)
 			if err != nil {
-				return false, nil, err
+				return err
 			}
 
-			if n1.Contains(n2.IP) || n2.Contains(n1.IP) {
-				return true, []string{c1, c2}, nil
+			_, c1e := processed[c1+c2]
+			_, c2e := processed[c2+c1]
+
+			if !c1e && !c2e && (n1.Contains(n2.IP) || n2.Contains(n1.IP)) {
+				fmt.Fprintf(os.Stdout, "CIDRs (%s) and (%s) overlap\n", c1, c2)
+
+				processed[c1+c2] = struct{}{}
+				processed[c2+c1] = struct{}{}
+
+				overlaps = true
 			}
 		}
-
 	}
 
-	return false, nil, nil
+	if !overlaps {
+		fmt.Fprintln(os.Stdout, "CIDRs do not overlap")
+	}
+
+	return nil
 }
